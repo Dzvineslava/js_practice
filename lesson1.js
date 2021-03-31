47c2d97a622358d5654af22c6b62db64a48a006b
Dzvinka <dzvineslava.slyzhuk@gmail.com>
2021-03-31 19:05:22 +0300

    second commit

--- lesson1.js
+++ lesson1.js
@@ -1,11 +1,11 @@
 alert(`Hello`)
 
-// const name = `Viktor`
-// let age = 24
-//
-// let age = 23
-//
-// let girl = "true"
-// age = "twenty five"
-//
-// alert("hello")
+const name = `Viktor`
+let age = 24
+
+let age = 23
+
+let girl = "true"
+age = "twenty five"
+
+alert("hello")
